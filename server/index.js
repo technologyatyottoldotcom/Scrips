d980d34a937d1b23ce204d65c826071306049b8b
Divyesh Kathe <53042607+divyeshkathe@users.noreply.github.com>
2021-03-20 14:22:30 +0530

    serving react app from server

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname,'../build')));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
   });
 
 app.listen(PORT,()=>{
